ca0238a84833901eacb5790a76b69df916533bc7
Rafael V. Suarez <rafaelv_s@hotmail.com>
2021-07-27 14:49:08 -0300

    Semeando a base de dados OnModelCreating

--- Cities.API/Migrations/20210727174113_SampleData.Designer.cs
+++ Cities.API/Migrations/20210727174113_SampleData.Designer.cs
@@ -3,14 +3,16 @@ using Cities.API.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cities.API.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20210727174113_SampleData")]
+    partial class SampleData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
