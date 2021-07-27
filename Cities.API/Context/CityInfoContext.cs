f747b2f2b116ef3d6f66be5ba7e003f508be0b83
Rafael V. Suarez <rafaelv_s@hotmail.com>
2021-07-27 13:17:21 -0300

    Entity Core - Criação do contexto e classes entity

---
+++ Cities.API/Context/CityInfoContext.cs
@@ -0,0 +1,25 @@
+﻿using Cities.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cities.API.Context
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterest { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+        public CityInfoContext(DbContextOptions<CityInfoContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Cities.API/Startup.cs
+++ Cities.API/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cities.API.Context;
 using Cities.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cities.API
@@ -27,6 +29,10 @@ namespace Cities.API
 #else
             services.AddTransient<IMailService,CloudMailService>();
 #endif
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=CityInfoDB;Trusted_Connection=True;";
+            services.AddDbContext<CityInfoContext>(o=> {
+                o.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
